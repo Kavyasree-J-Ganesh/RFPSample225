308b571a33439ec86b918f41e9f9bc8b9b30f520
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-11-12 16:52:59 +0530

    [Kavyasree J Ganesh] Java problems

--- src/Day1Problems/DisplayAMessage.java
+++
@@ -1,8 +0,0 @@
-package Day1Problems;
-
-public class DisplayAMessage {
-    public static void main(String[] args) {
-        String message = " I am Kavya ";
-        System.out.println(message);
-    }
-}
